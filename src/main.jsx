d898f723ac156d008ce75501fdcd169deee47dac
Beknazar <argus.m5.cs@gmail.com>
2024-10-03 19:16:57 +0600

    online-shop

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
-import myStore from './redux/store.js';
+import myStore from './locales/redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
